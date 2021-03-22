87db05fca06c33e8bffedf1f337aa45ea7709735
Nicholas Brancifort <brancifortnick@yahoo.com>
2021-03-22 19:16:47 -0400

    problem 3

--- problems/bsearch.js
+++ problems/bsearch.js
@@ -7,7 +7,6 @@ const evenNums = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 const empty = [];
 *******************************************************************/
 
-
 /*******************************************************************
 BINARY SEARCH VERSION 1:
 
@@ -18,7 +17,7 @@ targetNum is within the nums array.
 const recurBSearch = (nums, targetNum) => {
   // Base Case: if nums has no length, return false because we've run out of
   // items to search and haven't found targetNum
-  if (nums.length === 0){
+  if (nums.length === 0) {
     return false;
   }
   // determine the slice point (ie the 'middle' of the array).
@@ -26,24 +25,23 @@ const recurBSearch = (nums, targetNum) => {
   const ele = nums[middleIdx];
   // create "left half" and "right half" arrays, not including the slice point.
   const left = nums.slice(0, middleIdx);
-  const right = nums.slice(middleIdx + 1 , nums.length);
+  const right = nums.slice(middleIdx + 1, nums.length);
   // if targetNum is less than the value in the array at slice point,
   // return this search on the left half
-  if  (targetNum < ele)  {
+  if (targetNum < ele) {
     return recurBSearch(left, targetNum);
   }
   // if targetNum is greater than the value in the array at slice point,
   //return this search on the right half
-  else if  (targetNum > ele)  {
+  else if (targetNum > ele) {
     return recurBSearch(right, targetNum);
   }
   // if it's not greater than or less than (i.e. 'else'),
   // we know it's equal so return true
-  else  {
+  else {
     return true;
   }
-}
-
+};
 
 /*******************************************************************
 BINARY SEARCH VERSION 2:
@@ -55,28 +53,43 @@ targetNum is within the nums array.
 const iterBSearch = (nums, targetNum) => {
   // Save references to indices at the beginning, middle, and end of the array
   // into variables: lowerIdx, midIdx, and upperIdx
+  let lowerIdx = 0;
+  let midIdx = Math.floor(nums.length / 2);
+  let upperIdx = nums.length - 1;
 
-  // while the lowerIdx is less than or equal to the upperIdx, there are still
-  // values to be searched
+  while (lowerIdx <= upperIdx) {
+    midIdx = Math.floor((lowerIdx + upperIdx) / 2);
 
-  // reassign the midIdx to the the middle of the new lower and upper indices
-  // Hint: This is the sum of lower and upper, divided by 2
+    if (targetNum > nums[midIdx]) {
+      lowerIdx = midIdx + 1;
+    } else if (targetNum < nums[midIdx]) {
+      upperIdx = midIdx - 1;
+    } else {
+      return true;
+    }
+  }
+  return false;
+};
+//console.log(iterBSearch([1, 2, 3], 4));
+// while the lowerIdx is less than or equal to the upperIdx, there are still
+// values to be searched
 
-  // if targetNum is larger than the value in the middle, we know targetNum is
-  // not between the current lower and current middle, so reassign the lowerIdx
-  // to the middle (ie cut off the left half of the array)
+// reassign the midIdx to the the middle of the new lower and upper indices
+// Hint: This is the sum of lower and upper, divided by 2
 
-  // if targetNum is less than the value in the middle, we know targetNum is not
-  // between the current upper and current middle, so reassign the upperIdx
-  // to the middle (ie cut off the right half of the array)
+// if targetNum is larger than the value in the middle, we know targetNum is
+// not between the current lower and current middle, so reassign the lowerIdx
+// to the middle (ie cut off the left half of the array)
 
-  // if it's not greater than or less than (ie 'else'), we have found our target
-  // at the midIdx and can return true and stop iterating.
+// if targetNum is less than the value in the middle, we know targetNum is not
+// between the current upper and current middle, so reassign the upperIdx
+// to the middle (ie cut off the right half of the array)
 
-  // if we finish our while loop and haven't returned true, we've looked over
-  // the entire array and didn't find targetNum, so return false
-}
+// if it's not greater than or less than (ie 'else'), we have found our target
+// at the midIdx and can return true and stop iterating.
 
+// if we finish our while loop and haven't returned true, we've looked over
+// the entire array and didn't find targetNum, so return false
 
 /*******************************************************************
 BINARY SEARCH VERSION 3:
@@ -86,17 +99,32 @@ is in the nums array, and -1 if it is not found.
 *******************************************************************/
 
 const recurBSearchIdx = (nums, targetNum) => {
+  if (nums.length === 0) {
+    return -1;
+  }
+
+  const middleIdx = Math.floor(nums.length / 2);
+  const ele = nums[middleIdx];
+
+  const left = nums.slice(0, middleIdx);
+  const right = nums.slice(middleIdx + 1, nums.length);
+
+  if (targetNum < ele) {
+    recurBSearchIdx(left, targetNum);
+  } else if (targetNum > ele) {
+    recurBSearchIdx(right, targetNum);
+  } else {
+    return nums.indexOf(targetNum);
+  }
+};
   // this implementation is identical to version 1, except rather than
   // returning true/false, return the index where you found the item
   // (instead of true) or -1 (instead of false).
-
   // HINT: the index value you return should be the index in the ORIGINAL array
   // and not the index of the sliced array. You'll notice this problem arise
   // on the 'right half' recursion. in that, try saving the return value of the
   // recursive call into a variable, and adding it to the current stack-frame's
   // midIdx + 1.
-}
-
 
 /*******************************************************************
 BINARY SEARCH VERSION 4:
@@ -130,8 +158,7 @@ const recurBSearchIdxV2 = (nums, targetNum, low = null, hi = null) => {
   If it's not greater and not less (i.e. 'else'), return the slice point because
   we have found our value!
   */
-}
-
+};
 
 /*******************************************************************
 BINARY SEARCH VERSION 5:
@@ -143,12 +170,12 @@ it is in the nums array, and -1 if it is not found.
 const iterBSearchIdx = (nums, targetNum) => {
   // this is identical to Version 2, but return the index or -1 rather than
   // true or false
-}
+};
 
 module.exports = {
   recurBSearch,
   iterBSearch,
   recurBSearchIdx,
   recurBSearchIdxV2,
-  iterBSearchIdx
+  iterBSearchIdx,
 };
